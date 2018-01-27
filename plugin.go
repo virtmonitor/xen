d46bb69c5855c147d1c1b8d3077c5b8cc0e5d89f
Chris Norris <chris@norr.is>
2018-01-27 18:20:06 +0000

    Test Driver type

--- plugin.go
+++ plugin.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"github.com/virtmonitor/driver"
-
 	"github.com/virtmonitor/xen/driver"
 )
 
 //Driver driver backend
-var Driver driver.Driver = &xen.Xen{}
+var Driver interface{} = &xen.Xen{}
 
 func main() {}
