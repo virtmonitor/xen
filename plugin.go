bf5bfb75e81f7d0dffcf483ca70684d8f6bb1e1a
Chris Norris <chris@norr.is>
2019-01-08 03:07:24 +0000

    dgfdf

--- plugin.go
+++ plugin.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"os"
+
+	"github.com/hashicorp/go-hclog"
+
 	plugin "github.com/hashicorp/go-plugin"
 	"github.com/virtmonitor/plugins"
 	xen "github.com/virtmonitor/xen/driver"
 )
 
 func main() {
+	logger := hclog.New(&hclog.LoggerOptions{
+		Name:   "KVM",
+		Output: os.Stderr,
+	})
+
 	plugin.Serve(&plugin.ServeConfig{
 		HandshakeConfig: plugins.Handshake,
 		Plugins: map[string]plugin.Plugin{
 			"driver_grpc": &plugins.DriverGrpcPlugin{Impl: &xen.Xen{}},
 		},
 		GRPCServer: plugin.DefaultGRPCServer,
+		Logger:     logger,
 	})
 }
